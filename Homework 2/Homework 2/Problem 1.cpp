9d847590f55c7edb202c146e2f070a76848011ad
zachary-D <zacharyd1@live.com>
2017-09-12 22:35:04 -0400

    initial code, defined day constants

---
+++ Homework 2/Homework 2/Problem 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <string> 
+
+using std::string;
+using std::cout;
+using std::endl;
+
+namespace day //Weekday constants 
+{
+	const string d_m = "Monday";
+	const string d_tu = "Thursday";
+	const string d_w = "Wednesday";
+	const string d_th = "Thursday";
+	const string d_f = "Friday";
+};
+
+int main()
+{
+
+
+	return 0;
+}
